33a6190b5ca25120a4fc83108646850c6a8b9a55
Zain-Abbas <zain786@outlook.com>
2025-03-03 22:20:22 -0800

    in the process of adding point compression and complete graph with weighted edges where each weight is the distance between two 2 points (Towers)

--- src/FindMinCost.java
+++ src/FindMinCost.java
@@ -2,7 +2,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /*
  Test your code with the provided test cases in \tests
@@ -13,16 +13,197 @@ import java.util.StringTokenizer;
 */
 
 public class FindMinCost {
-    public static void main(String[] args) throws IOException {
-        BufferedReader f = new BufferedReader(new FileReader(args[0]));
-        StringTokenizer st = new StringTokenizer(f.readLine());
-        int n = Integer.parseInt(st.nextToken());
+    static int N; // size of thing
+    static int[] ss, qs;
 
-        for (int j = 0; j < n; j++) {
-            st = new StringTokenizer(f.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+    // Class that
+    class Edge {
+        private int v;
+        private int w;
+        private double weight; // weight of each edge
+
+        public Edge(int v, int w, double weight) {
+            this.v = v;
+            this.w = w;
+            this.weight = weight;
         }
+
+        public int either() {
+            return v;
+        }
+
+        public int other(int vertex) {
+            if (vertex == v) return w;
+            else if (vertex == w) return v;
+            else return -1;
+        }
+
+        public double weight() {
+            return weight;
+        }
+
+
+    }
+
+    class Bag<Item> implements Iterable<Item> {
+        private LinkedList<Item> items = new LinkedList<Item>();
+
+        public Bag() {
+            items = new LinkedList<>();
+        }
+
+        public void add(Item item) {
+            items.add(item);
+        }
+        public boolean isEmpty() {
+            return items.isEmpty();
+        }
+
+        public int size() {
+            return items.size();
+        }
+
+        @Override
+        public Iterator<Item> iterator() {
+            return items.iterator();
+        }
+    }
+    // create Graph Class that stores
+    // V: number of vertices (#of towers?)
+    //
+    class EdgeWeightedGraph {
+        private int V; // Number of vertices
+        private Bag<Edge>[]adj; // adjancency list
+        private int E;
+
+        public EdgeWeightedGraph(int V) {
+            this.V = V;
+            adj = (Bag<Edge>[]) new Bag[V];
+            for (int v = 0; v < V; v++) {
+                adj[v] = new Bag<Edge>();
+            }
+        }
+
+        public void addEdge(Edge e) {
+            int v = e.either();
+            int w = e.other(v);
+            adj[v].add(e);
+            adj[w].add(e);
+            E++;
+        }
+
+
+    }
+
+
+    // returns: [rank] -> [value in ps]
+    static HashMap<Integer, Integer> compressx(int[][] ps, int coord) {
+        for (int i = 0; i < N; i++)
+            // ps[i][0] -> retrieving x coordinate of each point
+            // ps[i][1] -> retrieving y coordinate of each point
+            // ex) ps[0][0] = 0, ps[1][0] = 2 ... etc)
+            qs[i] = ps[i][coord];
+
+        // //System.out.println("qs[i]: " + Arrays.toString(qs));
+        // sort the x and y coordinates
+        Arrays.sort(qs);
+        //  //System.out.println(" Sorted qs[i]: " + Arrays.toString(qs));
+        // create mapping of coordinates
+        // ex) m = {0: 0, 1:1}
+        var m = new HashMap<Integer, Integer>();
+        for (int i = 0; i < N; i++) {
+            // (populate map with sorted x and y coordinates)
+            Integer previousValue =  m.put(qs[i], i);
+            // //System.out.println("Inserted (" + qs[i] + ", " + i + "), previous value: " + previousValue);
+        }
+        return m;
+    }
+
+    // .[0] -> the scalar; .[1] -> order as found in original order
+    static void compress(int[][] points) {
+        qs = new int[N];
+        // compress both x and y coordinates
+        var xs = compressx(points, 0);
+        var ys = compressx(points, 1);
+        // Print xs
+        /*
+        //System.out.println("xs:");
+        for (Map.Entry<Integer, Integer> entry : xs.entrySet()) {
+            //System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+         */
+
+
+        /*
+        // Print ys
+        //System.out.println("ys:");
+        for (Map.Entry<Integer, Integer> entry : ys.entrySet()) {
+            //System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+         */
+
+
+        for (int i = 0; i < N; i++) {
+            // iterate and update each point to compressed x-y values
+            // i = 0
+            // xs.get(points[i][0]) -> look up x-coordinate in map to find value
+            // xs.get(points[0][i]) -> look up y-coordinate in map to find value
+            //  //System.out.println("points[" + i + "][0]: " + points[i][0]);
+            //  //System.out.println("points[" + i + "][1]: " + points[i][1]);
+            Integer xValue = xs.get(points[i][0]);
+            Integer yValue = ys.get(points[i][1]);
+            //  //System.out.println("compressed x-point: " + xValue + ", compressed y-point: " + yValue);
+            points[i][0] = xValue;
+            points[i][1] = yValue;
+        }
+        /*
+        for (int i = 0; i < N; i++) {
+            //System.out.println("Index: " + i + ", points[i][0]: " + points[i][0] + ", points[i][1]: " + points[i][1]);
+        }
+        //System.out.println();
+        */
+
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        var BufferedReader = new BufferedReader(new FileReader(args[0]));
+        // create StringBuilder object to build string
+        var build = new StringBuilder();
+        // store each line in file
+        var line = "";
+        //reading each line from file
+        while ((line = BufferedReader.readLine()) != null) {
+            //appending lines
+            build.append(line).append(" ");
+        }
+        BufferedReader.close();
+        //storing input as string of tokens
+        // ex ["5", "0", "3", "2","2","1","1","3","0","4","4"]
+        var tokens = build.toString().split(" ");
+        // //System.out.println(tokens[0]);
+        // tokens[0] = "5"
+        // parsing token (line) as Integer
+        // ex) 5 -> tells us number of points
+        N = Integer.parseInt(tokens[0]);
+        // create 2D array points
+        var points = new int[N][N];
+        // k = 1 -> start at line 1 to process each pair of points
+        // Integer.parseInt(tokens[k]) -> provides us value of points in given line
+        for (int i = 0, k = 1; i < N; i++) {
+            for (int j = 0; j < 2; j++, k++) {
+                // points[0][0] = 0
+                // points[0][1] = 3
+                points[i][j] = Integer.parseInt(tokens[k]);
+            }
+        }
+        compress(points);
+        // call edge-Weighted Graph:
+        // N -> number of points
+       // Graph g = new Graph(N);
 
         long answer= 0;
         /* your code here to calculate the answer*/
@@ -30,4 +211,7 @@ public class FindMinCost {
         System.out.println(answer);
     }
 
+    /// Plan: need a method that calculates the distance between 2 towers
+    ///  Tower a, Tower b
+    ///  Math.sqrt((a.x - b.x)) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
     }
